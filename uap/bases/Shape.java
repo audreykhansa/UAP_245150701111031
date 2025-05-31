8ca176c21399a01444258bd1d1785fbf83c19a42
Audrey Khansa Larasati <audreykhsalra@gmail.com>
2025-05-31 21:41:12 +0700

    Update Shape.java

--- uap/bases/Shape.java
+++ uap/bases/Shape.java
@@ -1,16 +1,17 @@
 package bases;
 
-import interfaces.*;
+public abstract class Shape {
+    private String name;
 
-public abstract class Shape implements ThreeDimensional, MassCalculable, MassConverter, ShippingCostCalculator, PiRequired {
-    protected static final double MASSA_JENIS = 8.0;
-
-    public double konversiKg(double massaGram) {
-        return massaGram / 1000.0;
+    public Shape() {
     }
 
-    public int biayaKirim(double massaKg) {
-        return (int) Math.ceil(massaKg) * 10000;
+    public void setName(String inputName) {
+        this.name = inputName;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public abstract void printInfo();
